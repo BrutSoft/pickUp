a2be9c881d3201f34815a6c6ec8b1690e62aef3a
Nathan Donolli <ndonolli@gmail.com>
2016-10-10 18:41:31 -0500

    add zivile's shit and change postinstall to "grunt"

--- src/server/server.js
+++ src/server/server.js
@@ -20,6 +20,14 @@ app.use(express.static(clientDir));
 app.post('/api/games', gameController.addRequest)
 console.log(`client directory: ${clientDir}`)
 
+// CORS
+app.use(function(req, res, next) {
+   res.header('Access-Control-Allow-Origin', "*");
+   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+   res.header('Access-Control-Allow-Headers', 'Content-Type');
+   next();
+});
+
 // Auth =======================================================================
 import User from './mongoose/models/users';
 app.use(passport.initialize());
