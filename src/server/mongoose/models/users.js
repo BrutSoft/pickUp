1d2ca1294183928938f8974c799b2036a650d614
David d'Aquin <david@daquin.com>
2016-10-10 16:33:04 -0500

    now encrypts passwords and checks the encrytion

--- src/server/mongoose/models/users.js
+++ src/server/mongoose/models/users.js
@@ -1,19 +1,37 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
+var bcrypt = require('bcrypt-nodejs');
 
 
 var User = new Schema({
-  username: String,
-  password: String
+  username: {type: String, required: true, index: {unique: true} },
+  password: {type: String, required: true}
 });
 
 User.plugin(passportLocalMongoose);
 
-User.methods.verifyPassword = function () {
+User.pre('save', function (next) {
+  var user = this;
+
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
+
+  //hash the password and set the field to be the hash.
+  bcrypt.hash(this.password, null, null, function (err, hash) {
+    if (err) {console.error(err); return next(err); }
+    user.password = hash;
+    next();
+  });
+
+});
+
+User.methods.verifyPassword = function (candidate) {
   console.log('verifying password...');
-  //TODO actually make a verification
-  return true;
+  //synch function. TODO refactor all this and server to be async.
+  console.log(candidate);
+  console.log(this.password);
+  return bcrypt.compareSync(candidate, this.password);
 }
 
 module.exports = mongoose.model('users', User);
