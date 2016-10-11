7090db0587993558c70bfbe78e8686b27dff5983
Nathan Donolli <ndonolli@gmail.com>
2016-10-11 10:56:08 -0500

    take out mongo uri hardcode

--- src/server/mongoose/dbConnect.js
+++ src/server/mongoose/dbConnect.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-const mongoUri = process.env.MONGODB_URI || 'mongodb://heroku_xhv7f19z:6bcii1ertp6ehkf7u9gfsmue44@ds053216.mlab.com:53216/heroku_xhv7f19z'
+const mongoUri = process.env.MONGODB_URI;
 mongoose.connect(mongoUri);
 const db = mongoose.connection;
 
