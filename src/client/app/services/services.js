c735fdce32d0d4e0ac7d2314e1626344d950574b
David d'Aquin <david@daquin.com>
2016-10-10 21:43:21 -0500

    services first draft

--- src/client/app/services/services.js
+++ src/client/app/services/services.js
@@ -26,4 +26,84 @@ angular.module('pickUp.services', [])
       property = val;
     }
   };
-});
+})
+
+.factory('AuthService',
+  ['$q', '$timeout', '$http',
+  function ($q, $timeout, $http) {
+
+    // create user variable
+    var user = null;
+
+    // return available functions for use in the controllers
+    return ({
+      isLoggedIn: function isLoggedIn() {
+        return user ? true : false;
+      },
+      getUserStatus: function getUserStatus() {
+        return user;
+      },
+      login: function login(username, password) {
+        var deferred = $q.defer();
+
+        $http.post('/login', {username: username, password: password})
+          .success(function (data, status) {
+            if (status === 200 && data.status){
+              user = true;
+              deferred.resolve();
+            } else {
+              user = false;
+              deferred.reject();
+            }
+          })
+          .error(function (data) {
+            user = false;
+            deferred.reject();
+          });
+        return deferred.promise;
+      },
+      logout: function logout() {
+        var deferred = $q.defer();
+
+        // send a get request to the server
+        $http.get('/user/logout')
+          // handle success
+          .success(function (data) {
+            user = false;
+            deferred.resolve();
+          })
+          // handle error
+          .error(function (data) {
+            user = false;
+            deferred.reject();
+          });
+
+        // return promise object
+        return deferred.promise;
+
+      },
+      register: function register() {
+        // create a new instance of deferred
+        var deferred = $q.defer();
+
+        // send a post request to the server
+        $http.post('/user/register',
+          {username: username, password: password})
+          // handle success
+          .success(function (data, status) {
+            if(status === 200 && data.status){
+              deferred.resolve();
+            } else {
+              deferred.reject();
+            }
+          })
+          // handle error
+          .error(function (data) {
+            deferred.reject();
+          });
+
+        // return promise object
+        return deferred.promise;
+      }
+    });
+}]);
